54b6fa1a0af8dcdc987f3d80ae9b958a918fe317
Vachira J <cyberwizard.dev@gmail.com>
2020-12-08 15:51:48 +0700

    first commit

---
+++ app/main_window.js
@@ -0,0 +1,25 @@
+const electron = require('electron');
+const { BrowserWindow } =  electron;
+
+class MainWindow extends BrowserWindow {
+    constructor(url) {
+        super({
+            width: 300,
+            height: 500,
+            frame: false,
+            resizable: false,
+            show: false,
+            webPreferences: {
+                backgroundThrottling: false
+            }
+        });
+        this.loadURL(url);
+        this.on('blur', this.onBlur.bind(this));
+    }
+
+    onBlur() {
+        this.hide();
+    }
+}
+
+module.exports = MainWindow;
